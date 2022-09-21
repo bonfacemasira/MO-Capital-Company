9ee45db3bd0b5d0c754f8c21293ea3219b225717
Bonface Masira <bonfacemasira@gmail.com>
2022-09-22 01:07:28 +0300

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
 import LandingPage from "./components/LandingPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <LandingPage />
+      <div></div>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,51 @@
 import React from "react";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function NavBar() {
   return (
-    <nav>
-      <header>MO-Capital Company</header>
-      <div>
-        <ul>
-          <li>HOME</li>
-          <li>FEATURES</li>
-          <li>PAGES</li>
-          <li>PROJECT</li>
-          <li>CONTACT</li>
-        </ul>
-      </div>
-    </nav>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">MO-Capital Company</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#features">FEATURES</Nav.Link>
+            <Nav.Link href="#pages">PAGES</Nav.Link>
+            <Nav.Link href="#project">PROJECT</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Dank memes
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+    //   <header>MO-Capital Company</header>
+    //   <div>
+    //     <ul>
+    //       <li>HOME</li>
+    //       <li>FEATURES</li>
+    //       <li>PAGES</li>
+    //       <li>PROJECT</li>
+    //       <li>CONTACT</li>
+    //     </ul>
+    //   </div>
   );
 }
 
